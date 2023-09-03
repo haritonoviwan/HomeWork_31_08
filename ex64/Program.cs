2d8aae1caac443a04070459e913e0a9e4b9c8bd1
Иван Харитонов <haritonoviwan@yandex.ru>
2023-09-03 13:22:11 +0300

    Сделал 64 задание

---
+++ ex64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+string numbers = NaturalNumbers(N, 1);
+Console.WriteLine(numbers);
+
+string NaturalNumbers(int n, int end)
+{
+    if (n == end) return end.ToString();
+    return n + " " + NaturalNumbers(n - 1, end);
+}
